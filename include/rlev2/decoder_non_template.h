0c632528ed1a778a89899989ab8b7ac3765fb288
shroudie <shunfan2@illinois.edu>
2020-08-03 19:53:10 -0500

    m

--- include/rlev2/decoder_non_template.h
+++ include/rlev2/decoder_non_template.h
@@ -1,5 +1,5 @@
 #include "utils.h"
-constexpr int DECODE_BUFFER_COUNT = 256;
+    constexpr int DECODE_BUFFER_COUNT = 256;
 constexpr int SHM_BUFFER_COUNT = DECODE_BUFFER_COUNT * BLK_SIZE;
 
     __global__ void decompress_func_non_template(const uint8_t* __restrict__ in, const uint64_t n_chunks, const blk_off_t* __restrict__ blk_off, const col_len_t* __restrict__ col_len, int64_t* __restrict__ out) {
@@ -123,9 +123,9 @@ constexpr int SHM_BUFFER_COUNT = DECODE_BUFFER_COUNT * BLK_SIZE;
 // iteration ++;
 // #endif
             auto mask = __activemask();
-			bool read;
+	    bool read;
 			#pragma unroll
-			for (int read_iter=0; read_iter<3; ++read_iter) {
+			for (int read_iter=0; read_iter<2; ++read_iter) {
 				
 				read = used_bytes + input_buffer_count < mychunk_size;
 				uint32_t read_sync = __ballot_sync(mask, read);
@@ -138,171 +138,171 @@ constexpr int SHM_BUFFER_COUNT = DECODE_BUFFER_COUNT * BLK_SIZE;
 				__syncwarp(mask);
 			}
 
+
 			if (!read_first) {
 				read_first = true;
 				first_byte = read_byte();
-				curr_fbw = get_decoded_bit_width((first_byte >> 1) & 0x1f);
+        		curr_fbw = get_decoded_bit_width((first_byte >> 1) & 0x1f);
 				curr_fbw_left = curr_fbw;
 				read_second = false;
 			}
 
-			// while (input_buffer_count >= 160 || !read) {
-				switch(first_byte & HEADER_MASK) {
-					case HEADER_SHORT_REPEAT: {
-						auto num_bytes = ((first_byte >> 3) & 0x07) + 1;
-						if (num_bytes <= input_buffer_count) { 
-							int64_t tmp_int = 0;
-							while (num_bytes-- > 0) {
-								tmp_int |= ((int64_t)read_byte() << (num_bytes * 8));
-							}
-							auto cnt = (first_byte & 0x07) + MINIMUM_REPEAT;
-							while (cnt-- > 0) {
-								*out_8B = tmp_int; 
-								out_8B += BLK_SIZE;
-							}
-							read_first = false;
-						} 
-					}	break;
-					case HEADER_DIRECT: {
-						if (!read_second) {
-							read_second = true;
-							curr_len = (((first_byte & 0x01) << 8) | read_byte()) + 1;
-							bits_left = 0; bits_left_over = 0; curr_64 = 0;
-						}
-						
+			switch(first_byte & HEADER_MASK) {
+			case HEADER_SHORT_REPEAT: {
+				auto num_bytes = ((first_byte >> 3) & 0x07) + 1;
+				if (num_bytes <= input_buffer_count) { 
+					int64_t tmp_int = 0;
+					while (num_bytes-- > 0) {
+						tmp_int |= ((int64_t)read_byte() << (num_bytes * 8));
+					}
+					auto cnt = (first_byte & 0x07) + MINIMUM_REPEAT;
+					while (cnt-- > 0) {
+						*out_8B = tmp_int; 
+						out_8B += BLK_SIZE;
+					}
+					read_first = false;
+				} 
+			}	break;
+			case HEADER_DIRECT: {
+				if (!read_second) {
+					read_second = true;
+					curr_len = (((first_byte & 0x01) << 8) | read_byte()) + 1;
+					bits_left = 0; bits_left_over = 0; curr_64 = 0;
+				}
+				
+				read_longs();
+				
+				if (curr_len <= 0) {
+					read_first = false;
+				}
+			}	break;
+			case HEADER_DELTA: {
+				if (!read_second) {
+					read_second = true;
+					curr_len = (((first_byte & 0x01) << 8) | read_byte()) + 1;
+					bits_left = 0; bits_left_over = 0; curr_64 = 0;
+					dal_read_base = false;
+				}
+
+				if (!dal_read_base && (input_buffer_count >= 64 + curr_fbw || !read)) {
+					base_val = read_uvarint();
+					base_delta = read_svarint();
+					write_int(base_val);
+					base_val += base_delta;
+					write_int(base_val);
+
+					base_out = out_8B;
+
+					dal_read_base = true;
+				}
+
+				if (dal_read_base) {
+					if (((first_byte >> 1) & 0x1f) != 0) {
+						// var delta
 						read_longs();
-						
-						if (curr_len <= 0) {
-							read_first = false;
-						}
-					}	break;
-					case HEADER_DELTA: {
-						if (!read_second) {
-							read_second = true;
-							curr_len = (((first_byte & 0x01) << 8) | read_byte()) + 1;
-							bits_left = 0; bits_left_over = 0; curr_64 = 0;
-							dal_read_base = false;
-						}
-
-						if (!dal_read_base) {
-							if (input_buffer_count < 64 + curr_fbw && read) break;
-							dal_read_base = true;
-							
-							base_val = read_uvarint();
-							base_delta = read_svarint();
-							write_int(base_val);
-							base_val += base_delta;
-							write_int(base_val);
-
-							base_out = out_8B;
-						}
-
-						if (((first_byte >> 1) & 0x1f) != 0) {
-							// var delta
-							read_longs();
-							if (curr_len <= 0) {
-								if (base_delta > 0) {
-									while (base_out < out_8B) {
-										base_val = (*base_out += base_val);
-										base_out += BLK_SIZE;
-									}
-								} else {
-									while (base_out < out_8B) {
-										base_val = (*base_out -= base_val);
-										base_out += BLK_SIZE;
-									}
-								}
-								read_first = false;
-							}
-						} else {
-							// fixed delta encoding
-							while (curr_len > 0) {
-								base_val += base_delta;
-								write_int(base_val);
-								curr_len --;
-							}
-							read_first = false;
-						}
-					}	break;
-					case HEADER_PACTED_BASE: {
-		// if (tid == ERR_THREAD) {
-		// 	printf("patched base case\n");
-		// }
-						//TODO Try to guarantee there are at least 4 btyes to read (all headers)
-						if (!read_second) {
-							
-							// if (input_buffer_count < 3) break;
-							// Not necessary because iteration starts with reading 4 bytes
-
-							read_second = true;
-
-							curr_len = (((first_byte & 0x01) << 8) | read_byte()) + 1;
-							auto third = read_byte();
-							auto forth = read_byte();
-
-							bw = ((third >> 5) & 0x07) + 1;
-							pw = get_decoded_bit_width(third & 0x1f);
-							pgw = ((forth >> 5) & 0x07) + 1;
-							pll = forth & 0x1f;
-
-							bits_left = 0; bits_left_over = 0; curr_64 = 0;
-
-							patch_gap = 0;
-
-							curr_pwb_left = get_closest_bit(pw + pgw);
-							dal_read_base = false;
-						}
-						
-
-						if (!dal_read_base) {
-							if (input_buffer_count < bw) break;
-							dal_read_base = true;
-							base_val = read_long(bw);
-							base_out = out_8B;
-						} 
-
-
-
-						read_longs();
-						if (curr_len <= 0) {
-							auto patch_mask = (static_cast<uint16_t>(1) << pw) - 1;
-							while (pll > 0) {
-								while (input_buffer_count > 0 && curr_pwb_left > bits_left) {
-									curr_64 <<= bits_left;
-									curr_64 |= bits_left_over & ((1 << bits_left) - 1);
-									curr_pwb_left -= bits_left;
-									bits_left_over = read_byte();
-									bits_left = 8;
-								}
-
-								if (curr_pwb_left <= bits_left) {
-									if (curr_pwb_left > 0) {
-										curr_64 <<= curr_pwb_left;
-										bits_left -= curr_pwb_left;
-										curr_64 |= (bits_left_over >> bits_left) & ((1 << curr_pwb_left) - 1);
-									}
-
-									patch_gap += curr_64 >> pw;
-									base_out[patch_gap * BLK_SIZE] |= static_cast<int64_t>(curr_64 & patch_mask) << curr_fbw;
-
-									pll --;
-									curr_64 = 0;
-									curr_pwb_left = get_closest_bit(pw + pgw);
-								} 
-								if (input_buffer_count <= 0) break;
-
-							}
-							if (pll <= 0) {
+                        if (curr_len <= 0) {
+							if (base_delta > 0) {
 								while (base_out < out_8B) {
-									*base_out += base_val;
+									base_val = (*base_out += base_val);
 									base_out += BLK_SIZE;
 								}
-								read_first = false; 
+							} else {
+								while (base_out < out_8B) {
+									base_val = (*base_out -= base_val);
+									base_out += BLK_SIZE;
+								}
 							}
+                            read_first = false;
+                        }
+					} else {
+						// fixed delta encoding
+						while (curr_len > 0) {
+							base_val += base_delta;
+							write_int(base_val);
+							curr_len --;
 						}
-					}	break;
+                        read_first = false;
+					}
 				}
-			// }
+			}	break;
+			case HEADER_PACTED_BASE: {
+// if (tid == ERR_THREAD) {
+// 	printf("patched base case\n");
+// }
+				//TODO Try to guarantee there are at least 4 btyes to read (all headers)
+				if (!read_second) {
+					
+                    // if (input_buffer_count < 3) break;
+                    // Not necessary because iteration starts with reading 4 bytes
+
+					read_second = true;
+
+					curr_len = (((first_byte & 0x01) << 8) | read_byte()) + 1;
+					auto third = read_byte();
+        			auto forth = read_byte();
+
+					bw = ((third >> 5) & 0x07) + 1;
+					pw = get_decoded_bit_width(third & 0x1f);
+					pgw = ((forth >> 5) & 0x07) + 1;
+					pll = forth & 0x1f;
+
+					bits_left = 0; bits_left_over = 0; curr_64 = 0;
+
+					patch_gap = 0;
+
+					curr_pwb_left = get_closest_bit(pw + pgw);
+					dal_read_base = false;
+				}
+				
+
+				if (!dal_read_base) {
+                    if (input_buffer_count < bw) break;
+					dal_read_base = true;
+					base_val = read_long(bw);
+					base_out = out_8B;
+				} 
+
+
+
+                read_longs();
+				if (curr_len <= 0) {
+					auto patch_mask = (static_cast<uint16_t>(1) << pw) - 1;
+					while (pll > 0) {
+						while (input_buffer_count > 0 && curr_pwb_left > bits_left) {
+							curr_64 <<= bits_left;
+							curr_64 |= bits_left_over & ((1 << bits_left) - 1);
+							curr_pwb_left -= bits_left;
+							bits_left_over = read_byte();
+							bits_left = 8;
+						}
+
+						if (curr_pwb_left <= bits_left) {
+							if (curr_pwb_left > 0) {
+								curr_64 <<= curr_pwb_left;
+								bits_left -= curr_pwb_left;
+								curr_64 |= (bits_left_over >> bits_left) & ((1 << curr_pwb_left) - 1);
+							}
+
+							patch_gap += curr_64 >> pw;
+							base_out[patch_gap * BLK_SIZE] |= static_cast<int64_t>(curr_64 & patch_mask) << curr_fbw;
+
+							pll --;
+							curr_64 = 0;
+							curr_pwb_left = get_closest_bit(pw + pgw);
+						} 
+						if (input_buffer_count <= 0) break;
+
+					}
+					if (pll <= 0) {
+						while (base_out < out_8B) {
+							*base_out += base_val;
+							base_out += BLK_SIZE;
+						}
+						read_first = false; 
+					}
+				}
+			}	break;
+			}
 
 // #ifdef DEBUG
 // if (iteration > max_iter) {
