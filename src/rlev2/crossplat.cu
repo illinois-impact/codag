35faa0d96bf54a3ba88af9f0049aaf166d6ec068
minerl2 <minerl2@illinois.edu>
2020-09-21 16:52:46 -0500

    templatize for testing

---
+++ src/rlev2/crossplat.cu
@@ -0,0 +1,110 @@
+#include <common.h>
+#include <iostream>
+#include <cstring>
+#include <sys/types.h> 
+#include <sys/stat.h> 
+#include <fcntl.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <chrono>
+
+#include <rlev2/rlev2.h>
+#include <cuda/atomic>
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr << "please provide arguments\n";
+        exit(1);
+    }
+
+    FILE* fp;
+    long lSize;
+    char* buffer;
+
+    fp = fopen(argv[1], "rb");
+    if (!fp) perror("fopen"), exit(EXIT_FAILURE);
+
+    fseek(fp, 0L, SEEK_END);
+    lSize = ftell(fp);
+    rewind(fp);
+
+    /* allocate memory for entire content */
+    buffer = (char*)malloc(lSize);
+    if (!buffer) fclose(fp), fputs("memory alloc fails", stderr), exit(EXIT_FAILURE);
+
+    /* copy the file into the buffer */
+    if (1 != fread(buffer, lSize, 1, fp))
+        fclose(fp), free(buffer), fputs("entire read fails", stderr), exit(EXIT_FAILURE);
+
+    uint8_t* encoded = nullptr;
+    uint64_t encoded_bytes = 0;
+
+    blk_off_t* blk_off;
+    col_len_t* col_len;
+    uint64_t n_chunks;
+
+        
+    int64_t* in = (int64_t*)buffer;
+
+    uint8_t *t_out;
+    printf("======> compress kernerl\n");
+    auto encode_start = std::chrono::high_resolution_clock::now();
+    rlev2::compress_gpu((int64_t*)in, lSize, t_out, encoded_bytes);
+
+    // rlev2::compress_gpu_transpose(in, lSize, encoded, encoded_bytes, n_chunks, blk_off, col_len);
+    auto encode_end = std::chrono::high_resolution_clock::now();
+
+    int64_t* decoded = nullptr;
+    uint64_t decoded_bytes = 0;
+
+    printf("======> decompress kernerl\n");
+    auto decode_start = std::chrono::high_resolution_clock::now();
+    rlev2::decompress_gpu((uint8_t*)t_out, encoded_bytes, decoded, decoded_bytes);
+        // rlev2::decompress_gpu(encoded, encoded_bytes, n_chunks, blk_off, col_len, decoded, decoded_bytes);
+    auto decode_end = std::chrono::high_resolution_clock::now();
+
+    auto decomp = std::chrono::duration_cast<std::chrono::duration<double>>(decode_end - decode_start);
+    std::cout << "decompression size: " << encoded_bytes << " bytes\n";
+    std::cout << "decompression time: " << decomp.count() << " secs\n";
+    // printf("exp(actual) %lu(%lu)\n",decoded_bytes, sizeof(ll));
+    // for (int i=0; i<n_digits; ++i) {
+    //     if (ll[i] != decoded[i]) {
+    //         printf("failed at %d\n", i);
+    //         break;
+
+    //     }
+    //     // printf("%ld : %ld\n", ll[i], decompressed[i]);
+    // }
+
+    printf("input size: %ld\n", lSize);   
+    assert(decoded_bytes == lSize);
+    printf("input ints: %ld\n", decoded_bytes / sizeof(int64_t));   
+
+    for (int i = 0; i < decoded_bytes / sizeof(int64_t); ++i) {
+        // printf("compare at %d %lld(%lld)\n", i, in[i], decoded[i]);
+        if (decoded[i] != in[i]) {
+            printf("fail at %d %lld(%lld)\n", i, in[i], decoded[i]);
+        }
+        assert(decoded[i] == in[i]);
+    }
+    
+    /*
+    for (int i = 0; i <128; ++i) {
+        if (decoded[i] != in[i]) {
+            printf("fail at %d %lld(%lld)\n", i, in[i], decoded[i]);
+        }
+    }
+    */
+
+    delete[] blk_off;
+    delete[] col_len;
+    delete[] encoded;
+    delete[] decoded;
+
+    fclose(fp);
+    free(buffer);
+
+}
